b77dbe0f40afaf930d456342560211a9bff57243
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2024-04-07 17:01:00 -0700

    Add files via upload

--- cse360project_milestone2/src/cse360project_milestone2/DisplayPatientInformation.java
+++ cse360project_milestone2/src/cse360project_milestone2/DisplayPatientInformation.java
@@ -1,33 +1,67 @@
-package cse360project_milestone2;
+package staffPortal;
 
 import java.util.Enumeration;
 import java.util.Hashtable;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javax.swing.JOptionPane;
+
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class DisplayPatientInformation {
 	private boolean editable=true;
-	public DisplayPatientInformation(boolean editable) {
+	public DisplayPatientInformation(boolean editable,Patient pfam) {
 		this.editable=editable;
+		this.kazouku=pfam;
+		setPatientData(kazouku);
 	}
-	//private Patient kazouku;
+	private Patient kazouku;
 	private Hashtable<String,String> data=new Hashtable<>();
-	//private void setPatientData(Patient pt) {
-			//data.put("CAC",pt.getCAC());
-	//}
+	private void setPatientData(Patient pt) {
+		    data.clear();
+		    data.put("first name",pt.getFirstName());
+		    data.put("last name",pt.getLastName());
+			data.put("username", pt.getUsername());
+			data.put("DOB",pt.getDOB());
+			data.put("email",pt.getEmail());
+			data.put("insurance provider",pt.getInsuranceProvider());
+			data.put("insurance id",Long.toString(pt.getInsuranceID()));
+			data.put("password",pt.getPassword());
+			data.put("address",pt.getAddress());
+			data.put("phone",StaffPortal.parsePhone(pt.getPhoneNumber()));
+			data.put("pharmacy", pt.getPharmacy());
+
+		
+	}
 	private void writeBack() {
-		//kazouku.putCAC(data.get("CAC"));
+		try {
+
+		kazouku.setPhoneNumber(Long.parseLong(data.get("phone").replace("-","")));
+		kazouku.setFirstName(data.get("first name"));
+		kazouku.setlastName(data.get("last name"));
+		kazouku.setDOB(data.get("DOB"));
+		kazouku.setEmail(data.get("email"));
+		kazouku.setInsuranceProvider(data.get("insurance provider"));
+		kazouku.setPassword(data.get("password"));
+		kazouku.setAddress(data.get("address"));
+		kazouku.setUsername(data.get("username"));
+		kazouku.setPharmacy(data.get("pharmacy"));
+		kazouku.setInsuranceID((int)Long.parseLong(data.get("insurance id")));
+		}catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(null, "Phone number and insurance id must be all digits and or dashes");
+		}
+		//write data to file
+		
 	}
 	
 	public void showWindow() 
 	{
+
 		GridPane contain=new GridPane();
 		Enumeration<String> keys=data.keys();
 		int row=0;
@@ -36,25 +70,32 @@ public class DisplayPatientInformation {
 			GridPane koto=new GridPane();
 			koto.add(new Label(key), 0, 0);
 			TextField jf=new TextField();
+			if(key.equals("password"))
+				jf=new PasswordField();
+			TextField copy=jf;
 			String value=data.get(key);
 			jf.setText(value);
 			koto.add(jf, 1, 0);
-			Button save=new Button("update");
-			save.setOnAction(new EventHandler<ActionEvent>() {
-
-					@Override
-					public void handle(ActionEvent arg0) {
-							data.put(key, jf.getText());
-							writeBack();
-					}});
-			if(editable)
-				koto.add(save, 2, 0);
+			
+			jf.setOnKeyPressed(e->{	
+					data.put(key, copy.getText());
+			});
+			jf.setOnKeyReleased(e->{
+				data.put(key, copy.getText());
+			});
+			
 			contain.add(koto, 0, row);
 			row++;
 		}
+		Button save=new Button("update");
+		save.setOnAction(e-> {writeBack();});
+	    if(editable)
+	    	contain.add(save, 0, row);
 		Alert alert = new Alert(AlertType.INFORMATION);
-		alert.getDialogPane().setExpandableContent(contain);
+
+		alert.getDialogPane().setContent(contain);
 		alert.showAndWait();
+		
 		
 	}
 }
--- cse360project_milestone2/src/cse360project_milestone2/StaffPortal.java
+++ cse360project_milestone2/src/cse360project_milestone2/StaffPortal.java
@@ -1,89 +1,120 @@
-package cse360project_milestone2;
+package staffPortal;
 
 import java.util.Enumeration;
 import java.util.Vector;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 
-public class StaffPortal extends Application{
-	 public static void main(String[] args) {
-	        launch(args);
-	    }
-	private Vector<String> patients=new Vector<>();
-	//private Vector<Patient> patients;
+public class StaffPortal{
+	//for testing
+	
+	private Vector<Patient> patients=new Vector<>();
 	
 	//public void addPatient(Patient pt) {
-	public void addPatient(String namae) {
-		
-		patients.add(namae);
-		//patients.add(pt);
+	public void addPatient(Patient pt) {
+
+		patients.add(pt);
 	}
-	
+	public void removePatient(Patient pt) {
+		patients.remove(pt);
+	}
+	public boolean findPatient(Patient pt) {
+		Enumeration<Patient> pe=patients.elements();
+		while(pe.hasMoreElements()) {
+			Patient p=pe.nextElement();
+			if(p.equals(pt))
+				return true;
+		}
+		return false;
+	}
 	
 	private String kensaku="";
 	private boolean editable=true;
+	//set the write permissions, true for yes
+	public void setWriteable(boolean writeable) {
+		editable=writeable;
+	}
+	private Label label(String test) {
+		Label l=new Label(test);
+		l.setStyle("-fx-padding: 16;");
+		return l;
+	}
+	public static String parsePhone(Long phone) {
+		String phones=Long.toString(phone);
+		if(phones.length()<7) 
+			return phones;
+		String d1=phones.substring(0,3);
+		String d2=phones.substring(3,6);
+		return d1+"-"+d2+"-"+phones.substring(6);
+	}
 	private void atarashi_risuto(GridPane ku) {
 		int row=1;
 		ku.getChildren().clear();
+		GridPane dcont=new GridPane();
 		kensaku_chihou_suru(ku);
-		Enumeration<String> risto=patients.elements();
+		dcont.setGridLinesVisible(true);
+		dcont.add(label("Patient Name"), 0, 0);
+		dcont.add(label("Date Of Birth"), 1, 0);
+	    dcont.add(label("Pharmacy"), 2, 0);
+	    dcont.add(label("Phone Number"), 3, 0);
+		Enumeration<Patient> risto=patients.elements();
+		
 		while(risto.hasMoreElements()) {
-			//Patient hito=risto.nextElement();
-			final String namae=risto.nextElement();
+			Patient hito=risto.nextElement();
+			final String namae=hito.getLastName()+", "+hito.getFirstName();
 			//namae=hito.getFullName();
 			if(namae.toLowerCase().contains(kensaku)) {
-					GridPane retsu=new GridPane();
-					Button rinku=new Button(namae);
-					
-				    rinku.setOnAction(new EventHandler<ActionEvent>() {
-
-							@Override
-							public void handle(ActionEvent arg0) {
-								DisplayPatientInformation info=new DisplayPatientInformation(editable);
-								
-								//info.showWindow(hito);
+					Hyperlink rinku=new Hyperlink(namae);
+					rinku.setStyle("-fx-padding: 16;");
+				    rinku.setOnAction(e-> {
+								DisplayPatientInformation info=new DisplayPatientInformation(editable,hito);
 								info.showWindow();
-							}});
-					//retsu.add(new Label(hito.getDOB()), 1, 0);
-				    //retsu.add(new Label(hito.getPharmacy()), 2, 0);
-				    //retsu.add(new Label(hito.getPhoneNumber()), 3, 0);
-					retsu.add(rinku, 0, 0);
-					ku.add(retsu, 0, row);
+							});
+					dcont.add(label(hito.getDOB()), 1, row);
+				    dcont.add(label(hito.getPharmacy()), 2, row);
+				    dcont.add(label(parsePhone(hito.getPhoneNumber())), 3, row);
+					dcont.add(rinku, 0, row);
+					
+					
 					row++;
 			}
 		}
+		ku.add(dcont, 0, 1);
 	}
+	private GridPane subete=new GridPane();
 	private void kensaku_chihou_suru(GridPane ku){
 		GridPane chihou=new GridPane();
-		TextField kensakuchihou=new TextField();
+		TextField kensakuchihou=new TextField("");
 		chihou.add(kensakuchihou, 0, 0);
-		Button suru=new Button("検索する");
-		suru.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent arg0) {
+		Button suru=new Button("search");
+		suru.setOnAction(e-> {
 				kensaku=kensakuchihou.getText();
-			}});
+				atarashi_risuto(subete);
+			});
 		chihou.add(suru, 1, 0);
 		ku.add(chihou, 0, 0);
 	}
-	@Override
-	public void start(Stage arg0) throws Exception {
+
+	public void start() {
 		// TODO Auto-generated method stub
-		addPatient("rufuria");
-		addPatient("yamamoto");
-		GridPane subete=new GridPane();
+		addPatient(launcher.readPatient("/home/ivan/Downloads/JDoe512241251.txt"));
+		
+		
+	
+		subete.setGridLinesVisible(true);
 		atarashi_risuto(subete);
-		arg0.setScene(new Scene(subete, 1000, 250));
-        arg0.show();
+		ScrollPane scroll=new ScrollPane(subete);
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.getDialogPane().setContent(scroll);
+		alert.setResizable(true);
+		alert.showAndWait();
 	}
 
 }
